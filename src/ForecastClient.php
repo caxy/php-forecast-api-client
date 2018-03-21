a2f2041331ddd6fcda9b15c2674ae4992da8c68e
Josh Schroeder <jschroed91@gmail.com>
2018-03-20 20:10:43 -0500

    Update forecast client to allow authentication using username/password

--- src/ForecastClient.php
+++ src/ForecastClient.php
@@ -6,25 +6,85 @@ use GuzzleHttp\Client;
 
 class ForecastClient
 {
+    const BASE_URI = 'https://api.forecastapp.com';
+    const AUTH_BASE_URI = 'https://id.getharvest.com';
+
     /**
      * @var Client
      */
     protected $guzzle;
 
     /**
+     * @var string
+     */
+    protected $apiToken;
+
+    /**
+     * @var string
+     */
+    protected $baseUri;
+
+    /**
+     * @var string|int
+     */
+    protected $accountId;
+
+    /**
      * ForecastClient constructor.
      */
-    public function __construct($accountId, $apiToken, $baseUri = 'https://api.forecastapp.com')
+    public function __construct($accountId, $apiToken = null, $baseUri = self::BASE_URI)
     {
-        $this->guzzle = new Client(
-            array(
-                'base_uri' => $baseUri,
-                'headers'  => array(
-                    'Forecast-Account-Id' => $accountId,
-                    'Authorization'       => 'Bearer '.$apiToken,
-                ),
-            )
-        );
+        $this->accountId = $accountId;
+        $this->apiToken = $apiToken;
+        $this->baseUri = $baseUri;
+        $this->guzzle = $this->makeClient();
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     *
+     * @return null|string the API access token.
+     * @throws \Exception
+     */
+    public function authenticate($username, $password)
+    {
+        $authClient = $this->makeAuthClient();
+        // Parse CSRF token from login form.
+        $loginFormResponse = $authClient->get('/forecast/sign_in');
+        $loginForm = $loginFormResponse->getBody()->getContents();
+        $matches = [];
+        $regexp = '/<input type="hidden" name="authenticity_token" value="(.*)"/';
+        preg_match_all($regexp, $loginForm, $matches);
+        $csrfToken = $matches[1][0];
+        if (empty($csrfToken)) {
+            throw new \Exception('CSRF token error - no token');
+        }
+        $data = [
+            'authenticity_token' => $csrfToken,
+            'email' => $username,
+            'password' => $password,
+            'product' => 'forecast'
+        ];
+        $sessionResponse = $authClient->post('/sessions', array('form_params' => $data));
+        // Don't follow redirects on this one?
+        $tokenRequestResponse = $authClient->get(sprintf('/accounts/%s', $this->accountId), array(
+            'allow_redirects' => false,
+        ));
+        $tokenRequest = $tokenRequestResponse->getBody()->getContents();
+        $matches = [];
+        $regexp = '/access_token\/(.*)\?"/';
+        preg_match_all($regexp, $tokenRequest, $matches);
+        if (isset($matches[1][0]) && !empty($matches[1][0])) {
+            $this->apiToken = $matches[1][0];
+
+            // Remake the client so that it uses the api token authorization header.
+            $this->guzzle = $this->makeClient();
+
+            return $this->apiToken;
+        } else {
+            throw new \Exception('Could not find token. Check your login, password and APP ID');
+        }
     }
 
     public function whoAmI()
@@ -97,6 +157,14 @@ class ForecastClient
         return $options;
     }
 
+    /**
+     * @param string $method
+     * @param string $uri
+     * @param array $options
+     *
+     * @return array
+     * @throws ForecastException
+     */
     protected function request($method, $uri, array $options = array())
     {
         $response = $this->guzzle->request($method, $uri, $options);
@@ -106,5 +174,37 @@ class ForecastClient
         }
 
         return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @return Client
+     */
+    protected function makeClient()
+    {
+        $headers = array('Forecast-Account-Id' => $this->accountId);
+
+        if ($this->apiToken) {
+            $headers['Authorization'] = sprintf('Bearer %s', $this->apiToken);
+        }
+
+        return new Client(
+            array(
+                'base_uri' => $this->baseUri,
+                'headers'  => $headers,
+            )
+        );
+    }
+
+    /**
+     * @return Client
+     */
+    protected function makeAuthClient()
+    {
+        return new Client(
+            array(
+                'base_uri' => self::AUTH_BASE_URI,
+                'cookies' => true,
+            )
+        );
     }
 }
